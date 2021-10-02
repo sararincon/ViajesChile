4da5ed0ad7272485a71f4b58ee6192d9bf226830
Sara Rincón Gonzalez <sararincon@MacBook-Air-de-Sara.local>
2021-10-02 16:22:49 -0300

    Tooltips

---
+++ assets/js/main.js
@@ -0,0 +1,19 @@
+//Tooltips
+var tooltipTriggerList = [].slice.call(
+  document.querySelectorAll('[data-bs-toggle="tooltip"]')
+);
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl);
+});
+
+//Cambiando color de nav
+
+$(function () {
+  $(document).scroll(function () {
+    var $nav = $(".fixed-top");
+
+    console.log("ASDASDASD", $nav);
+
+    $nav.toggleClass("scrolled", $(this).scrollTop() > 244);
+  });
+});
